cbefbc0b8d3e825a60249ae7ab027e010ac0c272
LukeGreeley18 <lukegreeley18@gmail.com>
2020-01-15 13:49:25 -0500

    adding new modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ShopsComponent } from './components/shops/shops.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "home", component: HomeComponent },
+  { path: "menu", component: MenuComponent },
+  { path: "shops", component: ShopsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  navigateHome() {
+    console.log('navigate home');
+    this.router.navigate(['/home']);
+  }
+
+  navigateMenu() {
+    console.log('navigate menu');
+    this.router.navigate(['/menu']);
+  }
+
+  navigateShops() {
+    console.log('navigate shops');
+    this.router.navigate(['/shops']);
+  }
+
+  navigateCoffee() {
+    console.log('navigate coffee');
+    window.open("https://pneumacoffee.com/", "_blank");
   }
 
 }
